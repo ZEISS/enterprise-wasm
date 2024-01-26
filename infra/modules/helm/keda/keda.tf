30d3fea7bb5eabf0400e4325e5c33aec9f64b491
Kai Walter <kai.walter@outlook.de>
2024-01-26 17:38:38 +0000

    try infinite Dapr maxConcurrentHandlers

--- infra/modules/helm/keda/keda.tf
+++ infra/modules/helm/keda/keda.tf
@@ -10,10 +10,43 @@ resource "helm_release" "keda" {
   chart      = "keda"
     namespace = kubernetes_namespace.keda.metadata.0.name
 
-  devel = "true"
-
-  set {
-    name  = "logLevel"
-    value = "debug"
-  }
+  values = [
+    <<-EOF
+    nodeSelector:
+      agentpool: ${var.keda_agentpool}
+    tolerations:
+      - key: "CriticalAddonsOnly"
+        operator: "Exists"
+    EOF
+  ]
 }
+
+# resource "helm_release" "keda_http" {
+#   name       = "keda-http-add-on"
+#   repository = "https://kedacore.github.io/charts"
+#   chart      = "keda-add-ons-http"
+#     namespace = kubernetes_namespace.keda.metadata.0.name
+#
+#   values = [
+#     <<-EOF
+#     operator:
+#       nodeSelector:
+#         agentpool: ${var.keda_agentpool}
+#       tolerations:
+#         - key: "CriticalAddonsOnly"
+#           operator: "Exists"
+#     scaler:
+#       nodeSelector:
+#         agentpool: ${var.keda_agentpool}
+#       tolerations:
+#         - key: "CriticalAddonsOnly"
+#           operator: "Exists"
+#     interceptor:
+#       nodeSelector:
+#         agentpool: ${var.keda_agentpool}
+#       tolerations:
+#         - key: "CriticalAddonsOnly"
+#           operator: "Exists"
+#     EOF
+#   ]
+# }
--- infra/modules/helm/keda/keda_variables.tf
+++ infra/modules/helm/keda/keda_variables.tf
@@ -3,3 +3,9 @@ variable "namespace" {
   default     = "keda-system"
   description = "Kubernetes namespace to create and install KEDA in"
 }
+
+variable "keda_agentpool" {
+  type        = string
+  default     = "default"
+  description = "Agent pool name to deploy KEDA to"
+}
