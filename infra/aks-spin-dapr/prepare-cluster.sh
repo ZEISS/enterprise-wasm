842f60aa075d1e117a50b16060227315bcffac0d
Kai Walter <kai.walter@outlook.de>
2024-02-18 09:19:54 +0000

    try spin-operator from local repo

--- infra/aks-spin-dapr/prepare-cluster.sh
+++ infra/aks-spin-dapr/prepare-cluster.sh
@@ -25,6 +25,33 @@ yq eval '. | select(.kind=="Deployment").spec.template.spec.nodeSelector={"agent
 kubectl apply -f -
 kubectl apply -f ./collector-config.yaml
 
-CLUSTER_NAME=`az resource list -g $RESOURCE_GROUP_NAME --resource-type Microsoft.ContainerService/managedClusters --query '[0].name' -o tsv`
+# CLUSTER_NAME=`az resource list -g $RESOURCE_GROUP_NAME --resource-type Microsoft.ContainerService/managedClusters --query '[0].name' -o tsv`
 
-az aks update -g $RESOURCE_GROUP_NAME -n $CLUSTER_NAME --kube-proxy-config kube-proxy.json
+# az aks update -g $RESOURCE_GROUP_NAME -n $CLUSTER_NAME --kube-proxy-config kube-proxy.json
+
+az acr login -n $AZURE_CONTAINER_REGISTRY_NAME
+
+pushd ../../../spin-operator/
+OPERATOR_REP=$AZURE_CONTAINER_REGISTRY_ENDPOINT/spin-operator
+OPERATOR_IMG=$OPERATOR_REP:latest
+# make docker-build docker-push IMG=$OPERATOR_IMG
+make install
+
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.2/cert-manager.yaml
+
+# kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.3/cert-manager.yaml
+# make deploy IMG=$OPERATOR_IMG
+
+kubectl apply -f spin-runtime-class.yaml
+helm upgrade --install spin-operator \
+  --namespace spin-operator \
+  --create-namespace \
+  --devel \
+  --wait \
+  --set controllerManager.manager.image.repository=$OPERATOR_REP \
+  --set certmanager.enabled=false \
+  --set certmanager.installCRDs=false \
+  ./charts/spin-operator
+  # oci://ghcr.io/spinkube/spin-operator
+
+popd
