e0e397cfc1cd56d0ccf560b620a8e00650d55f10
Kai Walter <kai.walter@outlook.de>
2024-03-06 06:19:08 +0000

    Express vs Spin equal

--- infra/aks-spin-dapr/variables.tf
+++ infra/aks-spin-dapr/variables.tf
@@ -76,7 +76,8 @@ variable "user_nodepools" {
     taints = []
     }, {
     name       = "wasm"
-    size       = "Standard_D2pds_v5"
+    # size       = "Standard_D2pds_v5"
+    size       = "Standard_DS2_v2"
     node_count = 3
     max_pods   = 250
     labels = {
