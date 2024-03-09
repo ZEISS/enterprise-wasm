af02397475ca22ae929b6451570c42449214026b
Kai Walter <kai.walter@outlook.de>
2024-03-09 16:29:30 +0000

    compare with no App scaling, same VM SKU

--- infra/aks-spin-dapr/variables.tf
+++ infra/aks-spin-dapr/variables.tf
@@ -76,7 +76,8 @@ variable "user_nodepools" {
     taints = []
     }, {
     name       = "wasm"
-    size       = "Standard_D2pds_v5"
+    # size       = "Standard_D2pds_v5"
+    size       = "Standard_D2s_v4"
     node_count = 3
     max_pods   = 250
     labels = {
